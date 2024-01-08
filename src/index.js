ca918adcc0472559f71bee57040866d41edf5f0a
muhammadqamar <muhammadqamar111@gmail.com>
2024-01-08 15:08:53 +0500

    fix error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter as Router } from 'react-router-dom' // Import BrowserRouter
 
 import App from './App'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { store } from './Store'
+
+import store from './Store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      {' '}
+      {/* Wrap your App with BrowserRouter */}
+      <App />
+    </Router>
   </Provider>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
